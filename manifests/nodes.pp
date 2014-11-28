466fd41009ae3479b64826307861c1e8793a1277
Alberto Di Savia Puglisi <disalberto@gmail.com>
2014-11-28 11:32:53 +0100

    revert sshd template

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,14 +3,14 @@ include puppet
 include hosts
 include user::virtual
 include sudoers
+include user::sysadmins
 
 node galera-master, default { 
   exec { "copy ssh keys":
-    command => "cp /root/prisma/modules/ssh/files/.ssh/* /home/prisma/.ssh/",
+    command => "cp /root/prisma/modules/ssh/files/.ssh/* /home/prisma/.ssh",
     path    => "/usr/local/bin/:/bin/",
   }
 }
 
 node galera-1, galera-2, galera-3 {
-  include user::sysadmins
 }
