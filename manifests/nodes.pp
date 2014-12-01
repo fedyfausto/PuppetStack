e56a9a6008d1a346be4c41fbdffc8ca2b0613126
Alberto Di Savia Puglisi <disalberto@gmail.com>
2014-12-01 12:31:24 +0100

    fix

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,13 +4,13 @@ include hosts
 include user::virtual
 include sudoers
 include user::sysadmins
-
-node galera-master, default { 
-  exec { "copy ssh keys":
-    command => "cp /root/prisma/modules/ssh/files/.ssh/* /root/.ssh",
-    path    => "/usr/local/bin/:/bin/",
-  }
+ 
+exec { "copy ssh keys":
+  command => "cp /root/prisma/modules/ssh/files/.ssh/* /root/.ssh",
+  path    => "/usr/local/bin/:/bin/",
 }
 
+node galera-master, default { 
+}
 node galera-1, galera-2, galera-3 {
 }
