cbdaba905ecb987577a198508bdbe8f4a02c4eae
Alberto Di Savia Puglisi <disalberto@gmail.com>
2014-12-01 11:53:12 +0100

    root does everything

--- modules/user/manifests/sysadmins.pp
+++ modules/user/manifests/sysadmins.pp
@@ -1,3 +1,4 @@
 class user::sysadmins {
-  realize(User::Virtual::Ssh_user['prisma'])
+#  realize(User::Virtual::Ssh_user['prisma'])
+  realize(User::Virtual::Ssh_user['root'])
 }
--- modules/user/manifests/virtual.pp
+++ modules/user/manifests/virtual.pp
@@ -1,32 +1,53 @@
 class user::virtual {
-  define ssh_user($key, $password, $sshkeytype = "ssh-rsa") {
-    $homedir = $kernel ? {
-      'SunOS' => '/export/home',
-      default => '/home', 
+  define ssh_user($key, $password = "galera", $sshkeytype = "ssh-rsa") {
+    if $name != 'root' {
+      $homedir = $kernel ? {
+        'SunOS' => '/export/home',
+        default => '/home', 
+      }
+      user { $name:     
+        ensure     => present,
+        managehome => true,
+        password   => sha1($password),
+      }
+      file { "${homedir}/${name}/.ssh":
+        ensure => directory,
+        mode   => '0700',
+        owner  => $name,
+        group  => $name,
+      }
+      ssh_authorized_key { "${name}_key":
+        key     => $key,
+        type    => $sshkeytype,
+        user    => $name,
+        ensure  => present,
+        require => File["${homedir}/${name}/.ssh"],
+      }
+      notice("The password is: ${password}, the user is ${name}, the home is ${homedir}, the ssh_key is ${key}")
+    } else {
+      $homedir = '/root' 
+      file { "${homedir}/.ssh": 
+        ensure => directory,
+        mode => '0755',
+        owner => 'root', 
+        group => 'root',
+      }
+      ssh_authorized_key { "root_key":
+        key     => $key,
+        type    => $sshkeytype,
+        user    => 'root',
+        ensure  => present,
+        require => File["${homedir}/.ssh"],
+      }
+      notice("The password is: ${password}, the user is ${name}, the home is ${homedir}, the ssh_key is ${key}")      
     }
-    user { $name:     
-      ensure     => present,
-      managehome => true,
-      password   => sha1($password),
-    }
-    file { "${homedir}/${name}/.ssh":
-      ensure => directory,
-      mode   => '0700',
-      owner  => $name,
-      group  => $name,
-    }
-    ssh_authorized_key { "${name}_key":
-      key     => $key,
-      type    => $sshkeytype,
-      user    => $name,
-      ensure  => present,
-      require => File["${homedir}/${name}/.ssh"],
-    }
-    notice("The password is: ${password}, the user is ${name}, the home is ${homedir}, the ssh_key is ${key}")
   }
-
+  @ssh_user { 'root':
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7llqR567sYc9APpodBgKJu00Kv9S7tzVBJ3Y6q4exds5Af5kkSLlv1mbJCOHl13pKJxN2qs9SzB02dl6wibO4u+m2gEyE5NsDBWwCdWffZxIJelfFGlLId4ZQKjWjL4I5cm3aaWvTexxViL10EaONEJK19WUkAP+bZ+3iApxRmHotT5G7TvL0CYchTCLbnrbG8nV4vjEUrGq+f+rw0U9ijBS/9KuH1UKbnj0t2EFtPJhTQPAXP4uR7Vj++L6EIg3ZPsT/+CrYDXcPxmNI1VDCtMg5cwicWz9mq5l5UGXpR0fyW15zV/E9l+lY9QcpzlQUIfYEDLOor7Ky1wBRP1Ix',
+  }
+  
   @ssh_user { 'prisma':
-    key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7llqR567sYc9APpodBgKJu00Kv9S7tzVBJ3Y6q4exds5Af5kkSLlv1mbJCOHl13pKJxN2qs9SzB02dl6wibO4u+m2gEyE5NsDBWwCdWffZxIJelfFGlLId4ZQKjWjL4I5cm3aaWvTexxViL10EaONEJK19WUkAP+bZ+3iApxRmHotT5G7TvL0CYchTCLbnrbG8nV4vjEUrGq+f+rw0U9ijBS/9KuH1UKbnj0t2EFtPJhTQPAXP4uR7Vj++L6EIg3ZPsT/+CrYDXcPxmNI1VDCtMg5cwicWz9mq5l5UGXpR0fyW15zV/E9l+lY9QcpzlQUIfYEDLOor7Ky1wBRP1Ix',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC7llqR567sYc9APpodBgKJu00Kv9S7tzVBJ3Y6q4exds5Af5kkSLlv1mbJCOHl13pKJxN2qs9SzB02dl6wibO4u+m2gEyE5NsDBWwCdWffZxIJelfFGlLId4ZQKjWjL4I5cm3aaWvTexxViL10EaONEJK19WUkAP+bZ+3iApxRmHotT5G7TvL0CYchTCLbnrbG8nV4vjEUrGq+f+rw0U9ijBS/9KuH1UKbnj0t2EFtPJhTQPAXP4uR7Vj++L6EIg3ZPsT/+CrYDXcPxmNI1VDCtMg5cwicWz9mq5l5UGXpR0fyW15zV/E9l+lY9QcpzlQUIfYEDLOor7Ky1wBRP1Ix',
     password => 'prisma',
   }
 }
