f879d632eac3dc2f870e0cd40b954ae43e82ed77
Alberto Di Savia Puglisi <disalberto@gmail.com>
2014-12-01 12:19:05 +0100

    prisma user partially bypassed

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,7 +7,7 @@ include user::sysadmins
 
 node galera-master, default { 
   exec { "copy ssh keys":
-    command => "cp /root/prisma/modules/ssh/files/.ssh/* /home/prisma/.ssh",
+    command => "cp /root/prisma/modules/ssh/files/.ssh/* /root/.ssh",
     path    => "/usr/local/bin/:/bin/",
   }
 }
