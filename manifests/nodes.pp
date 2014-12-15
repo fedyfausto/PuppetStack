18452149e266d2be540b44bb1ca812cc9f701143
root <root@galera-master.(none)>
2014-12-15 13:50:28 +0100

    fix exec path

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -12,22 +12,22 @@ include galera
 node galera-master, default { 
   exec { "copy ssh keys":
     command => "cp /root/prisma/modules/ssh/files/* /root/.ssh",
-    path    => "/usr/local/bin/:/bin/",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
   }
 
   exec { "start galera cluster":
-    command => "sudo service mysql start --wsrep-new-cluster",
-    path    => "/usr/local/bin/:/bin/",
+    command => "service mysql start --wsrep-new-cluster",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
   }  
 }
 node galera-1, galera-2, galera-3 {
   exec { "copy ssh auth_keys":
     command => "cp /root/prisma/modules/ssh/files/authorized_keys /root/.ssh",
-    path    => "/usr/local/bin/:/bin/",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
   }
 
   exec { "start galera cluster":
-    command => "sudo service mysql start",
-    path    => "/usr/local/bin/:/bin/",
+    command => "service mysql start",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
   }  
 }
