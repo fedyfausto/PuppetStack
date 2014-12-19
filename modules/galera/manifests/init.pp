91960b7259f3a9eaa4bba1fe87a5783cbf9fa0bf
root <root@galera-master.(none)>
2014-12-19 14:48:20 +0100

    test1

--- modules/galera/manifests/init.pp
+++ modules/galera/manifests/init.pp
@@ -1,8 +1,11 @@
 class galera {
 
-  include apt
-  
+  class apt_include {
+    include apt
+  }
+
   anchor { 'galera::begin': }                      ->
+    class { 'apt_include': }                       ->
     class { 'galera::dependencies': }              -> 
     class { 'galera::clusterconfig': }             -> 
   anchor { 'galera::end': }  
