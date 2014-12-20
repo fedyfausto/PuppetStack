ecbc2c58d952d9b9d0e48615e8ee8c576ac08eb7
Alberto Di Savia Puglisi <disalberto@gmail.com>
2014-12-20 19:40:47 +0100

    test11

--- modules/galera/manifests/exec.pp
+++ modules/galera/manifests/exec.pp
@@ -6,6 +6,10 @@ class galera::exec {
          command => "service mysql start --wsrep-new-cluster",
          path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
        }
+       exec { "grant privileges":
+         command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'debian-sys-maint\'@\'localhost\' IDENTIFIED BY \'Xt9JBkj4HOKK52AI\';\"",
+         path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+       }
     }  
 
     'galera-1','galera-2': {
