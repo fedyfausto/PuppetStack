0b50048d8041c710226ed45d481fa57ec300c48a
root <root@galera-master.(none)>
2015-01-08 12:08:32 +0100

    hosts init modded with hiera variables

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -1,24 +1,23 @@
 class hosts {
 
+  $ip_gal_m = hiera('ip_gal_m')
+  $ip_gal_1 = hiera('ip_gal_1')
+  $ip_gal_2 = hiera('ip_gal_2')
+
   host { 'galera-master':
-    ip => '10.55.1.150',
+    ip => $ip_gal_m,
     host_aliases => 'galera-master',
   }
 
   host { 'galera-1':
-    ip => '10.55.1.151',
+    ip => $ip_gal_1,
     host_aliases => 'galera-1',
   }
 
   host { 'galera-2':
-    ip => '10.55.1.152',
+    ip => $ip_gal_2,
     host_aliases => 'galera-2',
   }
-
-# host { 'galera-3':
-#   ip => '10.55.1.153',
-#   host_aliases => 'galera-3',
-# }
 
 }
 
