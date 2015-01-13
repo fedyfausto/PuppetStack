5decabb7753abd229416a9824522c8be720d6533
root <root@rabbit-1.(none)>
2015-01-13 13:44:49 +0100

    script fix

--- scripts/first_apply.sh
+++ scripts/first_apply.sh
@@ -6,7 +6,7 @@ sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get -y update && sudo apt-get -y install puppet
 
 # Ruby1.9.3 and librarian-puppet install
-sudo apt-get -y install ruby1.9.3 && gem install librarian-puppet
+sudo apt-get -y install ruby1.9.3 ruby-dev build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev && gem install librarian-puppet
 
 # Puppet apply custom script
 sh ~/prisma/modules/puppet/files/papply.sh
