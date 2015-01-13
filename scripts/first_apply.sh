7264f1b7119d20037f1ecc298ebd3e18455af847
root <root@rabbit-1.(none)>
2015-01-13 22:16:13 +0100

    script +

--- scripts/first_apply.sh
+++ scripts/first_apply.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Puppet install
-wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb -P / | wget resources/puppetlabs-release-precise.deb
+cd
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb -P / | wget ~/prisma/resources/puppetlabs-release-precise.deb
 sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get -y update && sudo apt-get -y install puppet
 
 # Ruby1.9.3 and librarian-puppet install
-sudo apt-get -y install ruby1.9.3 ruby-dev build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev && gem install librarian-puppet
+sudo apt-get -y install ruby1.9.3 build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev && gem install librarian-puppet
 
 # Puppet apply custom script
 sh ~/prisma/modules/puppet/files/papply.sh
