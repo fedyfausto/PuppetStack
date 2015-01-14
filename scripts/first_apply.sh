a1b0114b3e96e9909488437a1eaa76f43cf465b0
root <root@rabbit-3.(none)>
2015-01-14 22:55:19 +0100

    librarian-puppet with --no-ri and --no-rdoc options

--- scripts/first_apply.sh
+++ scripts/first_apply.sh
@@ -7,7 +7,7 @@ sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get -y update && sudo apt-get -y install puppet
 
 # Ruby1.9.3 and librarian-puppet install
-sudo apt-get -y install ruby1.9.3 build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev && gem install librarian-puppet
+sudo apt-get -y install ruby1.9.3 build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev && gem install librarian-puppet --no-ri --no-rdoc
 
 # Puppet apply custom script
 sh ~/prisma/modules/puppet/files/papply.sh
