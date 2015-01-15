ca007414744ee33bbd21663b9b5665b9110a17af
alberto88a <disalberto@gmail.com>
2015-01-15 17:04:28 +0100

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,37 +6,33 @@ include sudoers
 include user::sysadmins
 include stdlib
 
-$galera_nodes = hiera('galera_nodes')
-$hap1_priority = hiera('hap1_priority')
-$hap2_priority = hiera('hap2_priority')
-
 node galera-master {
   class { 'galera::master':
-    nodes_n => $galera_nodes,
+    nodes_n => hiera('galera_nodes'),
   }
 }
 
 node /(galera-)+[0-9]/ {
   class { 'galera::slave':
-    nodes_n => $galera_nodes,
+    nodes_n => hiera('galera_nodes'),
   }
 }
 
 node haproxy-1 {
   class { 'haproxy':
-    nodes_n  => $galera_nodes,
+    nodes_n  => hiera('galera_nodes'),
   }
   class { 'keepalived':
-    priority => $hap1_priority,
+    priority => hiera('hap1_priority'),
   }  
 }
 
 node haproxy-2 {
   class { 'haproxy':
-    nodes_n  => $galera_nodes,
+    nodes_n  => hiera('galera_nodes'),
   }
   class { 'keepalived':
-    priority => $hap2_priority,
+    priority => hiera('hap2_priority'),
   }
 }
 
