1e14ac8791068f27c94d2f320b89d80df1a81d98
alberto88a <disalberto@gmail.com>
2015-01-15 20:22:47 +0100

    Update galera.pp

--- modules/hosts/manifests/galera.pp
+++ modules/hosts/manifests/galera.pp
@@ -53,7 +53,44 @@ class hosts::galera {
         ip => $ip_gal_3,
         host_aliases => $hst_gal_3,
       }
-    
+    }'5': {
+      $ip_gal_m = hiera('ip_gal_m')
+      $ip_gal_1 = hiera('ip_gal_1')
+      $ip_gal_2 = hiera('ip_gal_2')
+      $ip_gal_3 = hiera('ip_gal_3')
+      $ip_gal_4 = hiera('ip_gal_4')
+      $hst_gal_m = hiera('hst_gal_m')  
+      $hst_gal_1 = hiera('hst_gal_1')  
+      $hst_gal_2 = hiera('hst_gal_2')  
+      $hst_gal_3 = hiera('hst_gal_3')  
+      $hst_gal_4 = hiera('hst_gal_4')  
+      
+      host { $hst_gal_m:
+        ip => $ip_gal_m,
+        host_aliases => $hst_gal_m,
+      }
+
+      host { $hst_gal_1:
+        ip => $ip_gal_1,
+        host_aliases => $hst_gal_1,
+      }
+
+      host { $hst_gal_2:
+        ip => $ip_gal_2,
+        host_aliases => $hst_gal_2,
+      }
+
+      host { $hst_gal_3:
+        ip => $ip_gal_3,
+        host_aliases => $hst_gal_3,
+      }
+      
+      host { $hst_gal_3:
+        ip => $ip_gal_3,
+        host_aliases => $hst_gal_3,
+      }
+    }
+  }  
 }
 
 include hosts::galera
