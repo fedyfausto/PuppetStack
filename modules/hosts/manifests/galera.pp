ada7dad7c7d6643eeab846aa15c712aeb3e024de
root <root@haproxy-3.(none)>
2015-01-15 20:20:02 +0100

    provv

---
+++ modules/hosts/manifests/galera.pp
@@ -0,0 +1,59 @@
+class hosts::galera {
+  
+  case hiera('galera_nodes') {
+    '3': {
+      $ip_gal_m = hiera('ip_gal_m')
+      $ip_gal_1 = hiera('ip_gal_1')
+      $ip_gal_2 = hiera('ip_gal_2')
+      $hst_gal_m = hiera('hst_gal_m')  
+      $hst_gal_1 = hiera('hst_gal_1')  
+      $hst_gal_2 = hiera('hst_gal_2')  
+  
+      host { $hst_gal_m:
+        ip => $ip_gal_m,
+        host_aliases => $hst_gal_m,
+      }
+
+      host { $hst_gal_1:
+        ip => $ip_gal_1,
+        host_aliases => $hst_gal_1,
+      }
+
+      host { $hst_gal_2:
+        ip => $ip_gal_2,
+        host_aliases => $hst_gal_2,
+      }
+    }
+    '4': {
+      $ip_gal_m = hiera('ip_gal_m')
+      $ip_gal_1 = hiera('ip_gal_1')
+      $ip_gal_2 = hiera('ip_gal_2')
+      $ip_gal_3 = hiera('ip_gal_3')
+      $hst_gal_m = hiera('hst_gal_m')  
+      $hst_gal_1 = hiera('hst_gal_1')  
+      $hst_gal_2 = hiera('hst_gal_2')  
+      $hst_gal_3 = hiera('hst_gal_3')  
+
+      host { $hst_gal_m:
+        ip => $ip_gal_m,
+        host_aliases => $hst_gal_m,
+      }
+
+      host { $hst_gal_1:
+        ip => $ip_gal_1,
+        host_aliases => $hst_gal_1,
+      }
+
+      host { $hst_gal_2:
+        ip => $ip_gal_2,
+        host_aliases => $hst_gal_2,
+      }
+
+      host { $hst_gal_3:
+        ip => $ip_gal_3,
+        host_aliases => $hst_gal_3,
+      }
+    
+}
+
+include hosts::galera
---
+++ modules/hosts/manifests/haproxy.pp
@@ -0,0 +1,45 @@
+class hosts::haproxy {
+  
+  case hiera('haproxy_nodes') {
+    '2': {
+      $ip_hap_1 = hiera('ip_hap_1')
+      $ip_hap_2 = hiera('ip_hap_2')
+      $hst_hap_1 = hiera('hst_hap_1')  
+      $hst_hap_2 = hiera('hst_hap_2')  
+  
+      host { $hst_hap_1:
+        ip => $ip_hap_1,
+        host_aliases => $hst_hap_1,
+      }
+
+      host { $hst_hap_2:
+        ip => $ip_hap_2,
+        host_aliases => $hst_hap_2,
+      }
+    }
+    '3': {
+      $ip_hap_1 = hiera('ip_hap_1')
+      $ip_hap_2 = hiera('ip_hap_2')
+      $ip_hap_3 = hiera('ip_hap_3')
+      $hst_hap_1 = hiera('hst_hap_1')  
+      $hst_hap_2 = hiera('hst_hap_2')  
+      $hst_hap_3 = hiera('hst_hap_3')  
+
+      host { $hst_hap_1:
+        ip => $ip_hap_1,
+        host_aliases => $hst_hap_1,
+      }
+
+      host { $hst_hap_2:
+        ip => $ip_hap_2,
+        host_aliases => $hst_hap_2,
+      }
+
+      host { $hst_hap_3:
+        ip => $ip_hap_3,
+        host_aliases => $hst_hap_3,
+      }
+    
+}
+
+include hosts::haproxy
