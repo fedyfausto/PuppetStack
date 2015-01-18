a125d2c56aabbf0a40d34fad9e0a6d3dac0938ba
alberto88a <disalberto@gmail.com>
2015-01-18 17:58:15 +0100

    Update key.pp

--- modules/glusterfs/manifests/key.pp
+++ modules/glusterfs/manifests/key.pp
@@ -4,7 +4,7 @@ class glusterfs::key {
     location    => 'http://ppa.launchpad.net/gluster/glusterfs-3.6/ubuntu',
     release     => 'precise',
     repos       => 'main',
-    key         => '4096R/3FE869A9',
+    key         => '0x13e01b7b3fe869a9',
     key_server  => 'keyserver.ubuntu.com',
     include_src => 'true',
     include_deb => 'true',
