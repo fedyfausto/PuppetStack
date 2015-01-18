ab48d2fd59cecc7e382db0557a5f16c2be02c9e6
alberto88a <disalberto@gmail.com>
2015-01-18 12:01:38 +0100

    glusterfs mod...still troubles

--- modules/glusterfs/manifests/mainserver.pp
+++ modules/glusterfs/manifests/mainserver.pp
@@ -2,12 +2,6 @@ class glusterfs::mainserver {
   
   require glusterfs::disk
 
-  $ip_glu_1 = hiera('ip_glu_1')
-  $ip_glu_2 = hiera('ip_glu_2')
-  $ip_glu_3 = hiera('ip_glu_3')
-  $ip_glu_4 = hiera('ip_glu_4')
-  $ip_glu_5 = hiera('ip_glu_5')
-  
   $mount_point = hiera('mount_point')
   $gluster_file = hiera('gluster_file')  
   $gluster_nodes = hiera('gluster_nodes')
@@ -15,18 +9,32 @@ class glusterfs::mainserver {
 
   case $gluster_nodes {
     '2': {
+      $ip_glu_1 = hiera('ip_glu_1')
+      $ip_glu_2 = hiera('ip_glu_2')
       $peer_probe = "gluster peer probe ${ip_glu_2}"
       $vol_create_opt = "replica 3 transport tcp ${ip_glu_1}:${brick} ${ip_glu_2}:${brick} force" 
     }
     '3': {
-      $peer_probe = "gluster peer probe ${ip_glu_2} | gluster peer probe ${ip_glu_3}"
+      $ip_glu_1 = hiera('ip_glu_1')
+      $ip_glu_2 = hiera('ip_glu_2')
+      $ip_glu_3 = hiera('ip_glu_3')
+      $peer_probe = "gluster peer probe ${ip_glu_3} | gluster peer probe ${ip_glu_2}"
       $vol_create_opt = "replica 3 transport tcp ${ip_glu_1}:${brick} ${ip_glu_2}:${brick} ${ip_glu_3}:${brick} force" 
     }
     '4': {
+      $ip_glu_1 = hiera('ip_glu_1')
+      $ip_glu_2 = hiera('ip_glu_2')
+      $ip_glu_3 = hiera('ip_glu_3')
+      $ip_glu_4 = hiera('ip_glu_4')
       $peer_probe = "gluster peer probe ${ip_glu_2} | gluster peer probe ${ip_glu_3} | gluster peer probe ${ip_glu_4}"  
       $vol_create_opt = "replica 3 transport tcp ${ip_glu_1}:${brick} ${ip_glu_2}:${brick} ${ip_glu_3}:${brick} ${ip_glu_4}:${brick} force" 
     }
     '5': {
+      $ip_glu_1 = hiera('ip_glu_1')
+      $ip_glu_2 = hiera('ip_glu_2')
+      $ip_glu_3 = hiera('ip_glu_3')
+      $ip_glu_4 = hiera('ip_glu_4')
+      $ip_glu_5 = hiera('ip_glu_5')
       $peer_probe = "gluster peer probe ${ip_glu_2} | gluster peer probe ${ip_glu_3} | gluster peer probe ${ip_glu_4} | gluster peer probe ${ip_glu_5}"  
       $vol_create_opt = "replica 3 transport tcp ${ip_glu_1}:${brick} ${ip_glu_2}:${brick} ${ip_glu_3}:${brick} ${ip_glu_4}:${brick} ${ip_glu_5}:${brick} force" 
     }
@@ -35,7 +43,7 @@ class glusterfs::mainserver {
   class { 'apt':
     always_apt_update => true,
   }
-
+  ->
   apt::ppa { 'ppa:semiosis/ubuntu-glusterfs-3.5': }
   ->
   package { ['glusterfs-server','glusterfs-client','glusterfs-common']: 
