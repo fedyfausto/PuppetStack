bceb308204819e3022722a769ba22aa4b53aee03
alberto88a <disalberto@gmail.com>
2015-01-21 21:09:02 +0100

    Update haproxy.pp

--- modules/database/manifests/haproxy.pp
+++ modules/database/manifests/haproxy.pp
@@ -2,6 +2,8 @@ class database::haproxy {
   
   $ip_hap_1 = hiera('ip_hap_1') 
   $ip_hap_2 = hiera('ip_hap_2') 
+  $ip_hap_3 = hiera('ip_hap_3') 
+
    
   exec { "clear haproxy users":
      command => "mysql -u root -e \"USE mysql; DELETE FROM mysql.user WHERE (User = \'haproxy_root\' OR User = \'haproxy_check\');\" ",
@@ -18,6 +20,11 @@ class database::haproxy {
      path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
      require => Exec["clear haproxy users"],
   }
+  exec { "user haproxy_check_hap_3":
+     command => "mysql -u root -e \"INSERT INTO mysql.user (Host,User) values (\'${ip_hap_3}\',\'haproxy_check\'); FLUSH PRIVILEGES;\" ",
+     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+     require => Exec["clear haproxy users"],
+  }
   exec { "user haproxy_root_hap_1":
      command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'haproxy_root\'@\'${ip_hap_1}\' IDENTIFIED BY \'password\' WITH GRANT OPTION; FLUSH PRIVILEGES;\" ",
      path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
@@ -25,6 +32,11 @@ class database::haproxy {
   }
   exec { "user haproxy_root_hap_2":
      command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'haproxy_root\'@\'${ip_hap_2}\' IDENTIFIED BY \'password\' WITH GRANT OPTION; FLUSH PRIVILEGES;\" ",
+     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+     require => Exec["clear haproxy users"],
+  }
+  exec { "user haproxy_root_hap_3":
+     command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'haproxy_root\'@\'${ip_hap_3}\' IDENTIFIED BY \'password\' WITH GRANT OPTION; FLUSH PRIVILEGES;\" ",
      path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
      require => Exec["clear haproxy users"],
   }
