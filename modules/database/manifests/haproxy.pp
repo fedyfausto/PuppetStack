7a1d98eb481192ebe2f34ec12ed8a8d64bf280ec
root <root@galera-master.ct.infn.it>
2015-01-22 13:15:04 +0100

    database test1

--- modules/database/manifests/haproxy.pp
+++ modules/database/manifests/haproxy.pp
@@ -1,6 +1,6 @@
 class database::haproxy {
 
-  $ip_hap_v = hiera('ip_hap_v')
+  $wildcard = hiera('wildcard')
 
   exec { "clear haproxy users":
     command => "mysql -u root -e \"USE mysql; DELETE FROM mysql.user WHERE (User = \'haproxy_root\' OR User = \'haproxy_check\');\" ",
@@ -8,12 +8,12 @@ class database::haproxy {
     require => Exec["start galera cluster"],
   }
   exec { "user haproxy_check":
-    command => "mysql -u root -e \"INSERT INTO mysql.user (Host,User) values (\'${ip_hap_v}\',\'haproxy_check\'); FLUSH PRIVILEGES;\" ",
+    command => "mysql -u root -e \"INSERT INTO mysql.user (Host,User) values (\'${wildcard}\',\'haproxy_check\'); FLUSH PRIVILEGES;\" ",
     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
     require => Exec["clear haproxy users"],
   }
   exec { "user haproxy_root":
-    command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'haproxy_root\'@\'${ip_hap_v}\' IDENTIFIED BY \'password\' WITH GRANT OPTION; FLUSH PRIVILEGES;\" ",
+    command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'haproxy_root\'@\'${wildcard}\' IDENTIFIED BY \'password\' WITH GRANT OPTION; FLUSH PRIVILEGES;\" ",
     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
     require => Exec["clear haproxy users"],
   }
