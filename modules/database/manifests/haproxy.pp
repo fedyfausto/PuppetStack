39ad0f833046631d112b26090c7faf50ab346850
alberto88a <disalberto@gmail.com>
2015-01-22 11:31:10 +0100

    Update haproxy.pp

--- modules/database/manifests/haproxy.pp
+++ modules/database/manifests/haproxy.pp
@@ -1,15 +1,15 @@
 class database::haproxy {
-  
-  exec { "clear haproxy users":
-    command => "mysql -u root -e \"USE mysql; DELETE FROM mysql.user WHERE (User = \'haproxy_root\' OR User = \'haproxy_check\');\" ",
-    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
-    require => Exec["start galera cluster"],
-  }
-  
+
   case $haproxy_nodes {
     '2': {
       $ip_hap_1 = hiera('ip_hap_1') 
       $ip_hap_2 = hiera('ip_hap_2') 
+
+      exec { "clear haproxy users":
+        command => "mysql -u root -e \"USE mysql; DELETE FROM mysql.user WHERE (User = \'haproxy_root\' OR User = \'haproxy_check\');\" ",
+        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+        require => Exec["start galera cluster"],
+      }
 
       exec { "user haproxy_check_hap_1":
         command => "mysql -u root -e \"INSERT INTO mysql.user (Host,User) values (\'${ip_hap_1}\',\'haproxy_check\'); FLUSH PRIVILEGES;\" ",
@@ -37,6 +37,11 @@ class database::haproxy {
       $ip_hap_2 = hiera('ip_hap_2') 
       $ip_hap_3 = hiera('ip_hap_3') 
 
+      exec { "clear haproxy users":
+        command => "mysql -u root -e \"USE mysql; DELETE FROM mysql.user WHERE (User = \'haproxy_root\' OR User = \'haproxy_check\');\" ",
+        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+        require => Exec["start galera cluster"],
+      }
       exec { "user haproxy_check_hap_1":
         command => "mysql -u root -e \"INSERT INTO mysql.user (Host,User) values (\'${ip_hap_1}\',\'haproxy_check\'); FLUSH PRIVILEGES;\" ",
         path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
