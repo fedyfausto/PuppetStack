ef8bd19b8e6ad82c35733ecd6c75d7000f750ced
alberto88a <disalberto@gmail.com>
2015-01-23 13:24:45 +0100

    Update init.pp

--- modules/hosts/manifests/init.pp
+++ modules/hosts/manifests/init.pp
@@ -1,7 +1,9 @@
 class hosts {
 
   $dns = hiera('dns')
-
+  $hst_puppet = hiera('hst_puppet')
+  $ip_puppet = hiera('ip_puppet')
+  
   host { "${hst_puppet}.${dns}":
     ip => $ip_puppet,
     host_aliases => $hst_puppet,
