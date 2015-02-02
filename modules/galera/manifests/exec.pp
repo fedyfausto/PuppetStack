a029de6ad5735bd7aefac1476ffb4ac4418c05b7
alberto88a <disalberto@gmail.com>
2015-02-02 21:25:02 +0100

    revert to previous galera::exec -> removed unnecessary code

--- modules/galera/manifests/exec.pp
+++ modules/galera/manifests/exec.pp
@@ -1,40 +1,17 @@
 class galera::exec::master {
-  case $osfamily {
-    'Debian': {
-      exec { "start galera cluster":
-        command => "service mysql start --wsrep-new-cluster",
-        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
-      }  
-      class { 'database':
-        require => Exec["start galera cluster"],
-      } 
-    }
-    'RedHat': {
-      exec { "start galera cluster":
-        command => "mysql bootstrap",
-        path    => "/etc/init.d",
-      }
-      class { 'database':
-        require => Exec["start galera cluster"],
-      }
-    }
+  exec { "start galera cluster":
+    command => "service mysql start --wsrep-new-cluster",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+  }  
+  class { 'database':
+    require => Exec["start galera cluster"],
   }
 }
 
 class galera::exec::slave {
-  case $osfamily {
-    'Debian': {
-      exec { "participate galera cluster":
-        command => "service mysql start",
-        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
-      }
-    }
-    'RedHat': {
-      exec { "participate galera gluster":
-        command => "mysql start",
-        path    => "/etc/init.d",
-      }
-    }
+  exec { "participate galera cluster":
+    command => "service mysql start",
+    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
   }
 } 
 
