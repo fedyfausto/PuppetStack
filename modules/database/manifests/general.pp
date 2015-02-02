d5e57331cd2de33d8a759de899e8aa700fece427
alberto88a <disalberto@gmail.com>
2015-02-02 11:45:28 +0100

    Update general.pp

--- modules/database/manifests/general.pp
+++ modules/database/manifests/general.pp
@@ -3,12 +3,22 @@ class database::general {
   $collation_server = hiera('collation_server')
   $init_connect = hiera('collation_server')
   $character_set_server = hiera('character_set_server')
-
-  exec { "grant privileges":
-    command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'debian-sys-maint\'@\'localhost\' IDENTIFIED BY \'Xt9JBkj4HOKK52AI\';\" ",  
-    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+  
+  case $osfamily {
+    'Debian': {
+      exec { "grant privileges":
+        command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'debian-sys-maint\'@\'localhost\' IDENTIFIED BY \'Xt9JBkj4HOKK52AI\';\" ",  
+        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+      }
+    }
+    'RedHat': {
+      exec { "grant privileges":
+        command => "mysql -u root -e \"GRANT ALL PRIVILEGES ON *.* TO \'root\'@\'%\' IDENTIFIED BY \'password\';\" ",  
+        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+      }
+    }
   }
-
+  
   file { 'my.cnf':
     path    => hiera('my_cnf_path'),
     content => template('database/my.erb'),
