f918781e604e14fc8f9193550adf6bcdd49dde3e
alberto88a <disalberto@gmail.com>
2015-02-03 21:07:27 +0100

    setenforce 0

--- modules/galera/manifests/exec.pp
+++ modules/galera/manifests/exec.pp
@@ -1,14 +1,29 @@
 class galera::exec::master {
+  if $osfamily == "RedHat" {
+    exec { "enforcing mode":
+      command => "sudo setenforce 0",
+      path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+    }
+  }
+  -> 
   exec { "start galera cluster":
     command => "service mysql start --wsrep-new-cluster",
     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
-  }  
+  }
+  ->
   class { 'database':
     require => Exec["start galera cluster"],
   }
 }
 
 class galera::exec::slave {
+  if $osfamily == "RedHat" {
+    exec { "enforcing mode":
+      command => "sudo setenforce 0",
+      path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
+    }
+  }
+  ->
   exec { "participate galera cluster":
     command => "service mysql start",
     path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/",
