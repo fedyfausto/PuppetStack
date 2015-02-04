ed4a5301d855e967a5565e42353df9cd0ceecbe3
alberto88a <disalberto@gmail.com>
2015-02-04 19:32:40 +0100

    Update and rename iptables.sh to lokkit.sh

--- modules/galera/files/iptables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp --dport 4444 -j ACCEPT
-iptables -A INPUT -p tcp --dport 4567 -j ACCEPT
-iptables -A INPUT -p tcp --dport 4568 -j ACCEPT
