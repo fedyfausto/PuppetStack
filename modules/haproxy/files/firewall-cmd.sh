1e564941245f6b91e2e3b73619ed510fdd67c30b
alberto88a <disalberto@gmail.com>
2015-02-05 12:52:53 +0100

    Update firewall-cmd.sh

--- modules/haproxy/files/firewall-cmd.sh
+++ modules/haproxy/files/firewall-cmd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-firewall-cmd --permanent --zone=public --add-port=80/tcp
-firewall-cmd --permanent --zone=public --add-port=3306/tcp
-firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=3306/tcp
+firewall-cmd --permanent --add-port=8080/tcp
 
-firewall-cmd --permanent --zone=public --add-port=8/tcp
-firewall-cmd --permanent --zone=public --add-port=112/tcp
+#firewall-cmd --permanent --zone=public --add-port=8/tcp
+#firewall-cmd --permanent --zone=public --add-port=112/tcp
 firewall-cmd --reload
