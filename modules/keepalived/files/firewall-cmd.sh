bdf8ba8fd33723b52517ccaab2f731e535bc65b0
alberto88a <disalberto@gmail.com>
2015-02-08 22:52:00 +0100

    Update firewall-cmd.sh

--- modules/keepalived/files/firewall-cmd.sh
+++ modules/keepalived/files/firewall-cmd.sh
@@ -1,11 +1,11 @@
-#firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i eth0 -d 224.0.0.0/8 -j ACCEPT
-#firewall-cmd --direct --perm --add-rule ipv4 filter INPUT 0 -i eth0 -d 224.0.0.0/8 -j ACCEPT
-#firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p vrrp -i eth0 -j ACCEPT
-#firewall-cmd --direct --perm --add-rule ipv4 filter INPUT 0 -p vrrp -i eth0 -j ACCEPT
-#firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -p vrrp -o eth0 -j ACCEPT
-#firewall-cmd --direct --perm --add-rule ipv4 filter OUTPUT 0 -p vrrp -o eth0 -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -i eth0 -d 224.0.0.0/8 -j ACCEPT
+firewall-cmd --direct --perm --add-rule ipv4 filter INPUT 0 -i eth0 -d 224.0.0.0/8 -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter INPUT 0 -p vrrp -i eth0 -j ACCEPT
+firewall-cmd --direct --perm --add-rule ipv4 filter INPUT 0 -p vrrp -i eth0 -j ACCEPT
+firewall-cmd --permanent --direct --add-rule ipv4 filter OUTPUT 0 -p vrrp -o eth0 -j ACCEPT
+firewall-cmd --direct --perm --add-rule ipv4 filter OUTPUT 0 -p vrrp -o eth0 -j ACCEPT
 
-firewall-cmd --permanent --zone=public --add-port=112/tcp
-firewall-cmd --permanent --zone=trusted --add-source=224.0.0.0/8
+#firewall-cmd --permanent --zone=public --add-port=112/tcp
+#firewall-cmd --permanent --zone=trusted --add-source=224.0.0.0/8
 
 firewall-cmd --reload
