97ed976147f33336ddc5d70d1d0b05d28c518a94
alberto88a <disalberto@gmail.com>
2015-02-11 13:36:24 +0100

    Update firewall-cmd.sh

--- modules/rabbit/files/firewall-cmd.sh
+++ modules/rabbit/files/firewall-cmd.sh
@@ -3,5 +3,6 @@
 firewall-cmd --permanent --add-port=5672/tcp
 firewall-cmd --permanent --add-port=4369/tcp
 firewall-cmd --permanent --add-port=35197/tcp
+firewall-cmd --permanent --add-port=25672/tcp
 firewall-cmd --reload
 setsebool -P nis_enabled 1
