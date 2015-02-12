7ed4382aa256b8abebb6233ca2930e0c2f04beaf
alberto88a <disalberto@gmail.com>
2015-02-12 15:43:34 +0100

    Update firewall-cmd.sh

--- modules/glusterfs/files/firewall-cmd.sh
+++ modules/glusterfs/files/firewall-cmd.sh
@@ -2,7 +2,7 @@
 firewall-cmd --permanent --add-port=111/tcp
 firewall-cmd --permanent --add-port=111/udp
 firewall-cmd --permanent --add-port=2049/tcp
-firewall-cmd --permanent --add-port=24007/tcp
+firewall-cmd --permanent --add-port=24007-24008/tcp
 firewall-cmd --permanent --add-port=38465-38469/tcp
-firewall-cmd --permanent --add-port=49152/tcp
+firewall-cmd --permanent --add-port=49152-49155/tcp
 firewall-cmd --reload
