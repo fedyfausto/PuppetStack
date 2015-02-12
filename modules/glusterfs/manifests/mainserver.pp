a6cd0222beead2c6ec0de45b19d80cdc9d548b8e
alberto88a <disalberto@gmail.com>
2015-02-12 17:25:30 +0100

    Update mainserver.pp

--- modules/glusterfs/manifests/mainserver.pp
+++ modules/glusterfs/manifests/mainserver.pp
@@ -84,7 +84,7 @@ class glusterfs::mainserver {
       ### Firewalld ###
       exec { "enforcing mode":
         command => "sudo setenforce 0",
-        path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/",
+        path    => "/usr/sbin/",
         notify  => Service['firewalld'],
       }
       service { 'firewalld':
@@ -112,20 +112,20 @@ class glusterfs::mainserver {
 
   exec { "gluster peer probe":
     command => $peer_probe,
-    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/",
+    path    => "/usr/sbin/",
    # require => Service['glusterfs-server'],
   }
 
   exec { "gluster volume create":
     command => "gluster volume create ${gluster_file} ${vol_create_opt}",
-    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/", 
+    path    => "/usr/sbin/", 
     require => Exec["gluster peer probe"],
   }   
   
   exec { "gluster volume start":
     command => "gluster volume start ${gluster_file}",
     unless  => "[ \"`gluster volume info ${gluster_file} | egrep '^Status:'`\" = 'Status: Started' ]",
-    path    => "/usr/local/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/", 
+    path    => "/usr/sbin/", 
     require => Exec["gluster volume create"],
   }
     
